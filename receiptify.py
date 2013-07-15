0cd4cac06da817de2cad05bcccbb59bf866c55d0
Daniel Greenfeld <pydanny@gmail.com>
2013-07-15 11:08:56 +0200

    Python is back in

---
+++ receiptify.py
@@ -0,0 +1,11 @@
+import envoy
+import sys
+
+
+if __name__ == "__main__":
+    filename = sys.argv[-1]
+    if filename.endswith("receiptify.py"):
+        print("ERROR! Please enter a ReStructuredText filename!")
+        sys.exit()
+    envoy.run("simplicity receipts.rst")
+    envoy.run("complexity")
